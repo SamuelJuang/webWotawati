00757b905fbd1007ece2e338302a338f115ac41e
SamuelJuang <samueljuang007@gmail.com>
2024-07-20 15:14:14 +0700

    text small fix

--- src/components/WotawatiShortMessage.jsx
+++ src/components/WotawatiShortMessage.jsx
@@ -54,13 +54,13 @@ const WotawatiShortMessage = forwardRef((props, ref) => {
           "linear-gradient(to bottom, transparent 95%, rgb(17,25,36) 100%)",
       }}
     >
-       <div className="w-full lg:w-7/12 h-1/4 lg:h-full">
+      <div className="w-full lg:w-7/12 h-1/4 lg:h-full">
         <CarouselDefault />
       </div>
       <div className="h-3/4 lg:h-full lg:w-1/2 flex justify-center items-center p-5 lg:me-5">
         <div className="flex flex-col items-start lg:justify-center lg:items-center lg:me-5">
-          <div className="text-2xl pt-5 lg:text-4xl font-bold text-center font-poppins flex flex-row gap-2">
-            <h1 className="text-center">Destinasi wisata</h1>
+          <div className="text-xl pt-5 lg:text-4xl font-bold text-center font-poppins flex flex-row gap-2 justify-center items-center">
+            <h1>Destinasi wisata</h1>
             <h1
               className="text-secondary"
               style={{
@@ -76,20 +76,26 @@ const WotawatiShortMessage = forwardRef((props, ref) => {
             </h1>
             <h1>yang</h1>
           </div>
-          <p className="text-2xl lg:text-4xl font-bold text-center font-poppins">
+          <p className="text-xl lg:text-4xl font-bold text-center font-poppins">
             memukau
           </p>
-          <p className="text-2xl lg:text-4xl font-bold text-center font-poppins">
+          <p className="text-xl lg:text-4xl font-bold text-center font-poppins">
             di Girisubo, Gunungkidul!
           </p>
-          <p className="flex lg:hidden pt-5 font-inter">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur id diam sapien. Suspendisse potenti. Sed vulputate felis mauris, non pellentesque metus porttitor quis. Sed commodo dapibus est in malesuada. Duis tempor dapibus ultricies. Sed sit amet ullamcorper lorem. Ut tellus felis, imperdiet eu diam at, pharetra bibendum nibh. Nunc massa nisl, luctus vitae ultrices eu, tincidunt sed nunc. Donec semper, metus a tristique rhoncus, velit nisi iaculis velit, sed maximus urna neque id tellus. Nunc vel lectus ullamcorper, vestibulum elit sed, blandit felis. Pellentesque a metus blandit, egestas tortor at, pharetra neque.
-
+          <p className="text-justify block lg:hidden pt-5 font-inter">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+            id diam sapien. Suspendisse potenti. Sed vulputate felis mauris, non
+            pellentesque metus porttitor quis. Sed commodo dapibus est in
+            malesuada. Duis tempor dapibus ultricies. Sed sit amet ullamcorper
+            lorem. Ut tellus felis, imperdiet eu diam at, pharetra bibendum
+            nibh. Nunc massa nisl, luctus vitae ultrices eu, tincidunt sed nunc.
+            Donec semper, metus a tristique rhoncus, velit nisi iaculis velit,
+            sed maximus urna neque id tellus. Nunc vel lectus ullamcorper,
+            vestibulum elit sed, blandit felis. Pellentesque a metus blandit,
+            egestas tortor at, pharetra neque.
           </p>
         </div>
       </div>
-
-     
     </div>
   );
 });
