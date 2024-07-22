bf30e7207cb53880c9fd2eaea9480ed366478302
SamuelJuang <samueljuang007@gmail.com>
2024-07-22 10:33:20 +0700

    carousel revision, read more coming soon

--- src/components/HistorySection.jsx
+++ src/components/HistorySection.jsx
@@ -1,25 +1,18 @@
 import Cube from "./Cube";
+import { CarouselDefault } from "./flickingCarousel";
+import { CarouselWithContent } from "./HistoryCarousel";
+
+
 
 const HistorySection = () => {
   return (
-    <section>
-      <div className="flex flex-col-reverse md:flex-row mb-[5rem] ">
-        <div className="relative flex w-full md:w-7/12 pe-[5rem] items-center justify-center">
-          <div className="">
-          <Cube />
-          </div>
-        </div>
-
-        <div className="flex flex-col relative w-full md:w-5/12 lg:me-5 justify-center items-center text-center lg:text-left">
-          
-          <p className="font-poppins text-4xl lg:text-5xl font-bold mb-5">
-                Sejarah Singkat Wotawati
-              </p>
-              <p className="font-inter text-sm lg:text-xl font-light z-10">
-                Kepala Dukuh adalah seorang yang memimpin di sebuah daerah dalam
-                tingkat dukuh. Seorang Kepala Dukuh bertanggung jawab untuk
-                mengelola pemerintahan dan kesejahteraan masyarakat di dukuhnya.
-              </p>
+    <section className="h-screen flex flex-col justify-center items-center">
+      <div className="flex flex-col relative w-full lg:me-5 justify-center items-center text-center lg:text-left">
+        <p className="font-poppins text-4xl lg:text-5xl font-bold">
+          Sejarah Singkat Wotawati
+        </p>
+        <div className="h-3/4 w-full lg:w-3/4">
+          <CarouselWithContent/>
         </div>
       </div>
     </section>
