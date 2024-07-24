18a4e89d950d361832192081662409cae1475c1b
SamuelJuang <samueljuang007@gmail.com>
2024-07-24 12:08:55 +0700

    added mapping

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -6,6 +6,7 @@ import Navbar from "../components/navbar";
 import PakDukuhShowcase from "../components/PakDukuhShowcase";
 import HistorySection from "../components/HistorySection";
 import Footer from "../components/Footer";
+import mapping from "../assets/mapping (1).webp";
 
 const AboutPage = () => {
   const lenisRef = useRef();
@@ -23,16 +24,19 @@ const AboutPage = () => {
   });
   return (
     <ReactLenis root>
-    <Navbar/>
-    <PakDukuhShowcase />
-    <div
-        className="absolute z-0 w-[15rem] aspect-square bg-primary rounded-full top-[150%] right-0 translate-x-[-50%] translate-y-[-50%] blur-[8rem]"
-        ></div>
-        <div
-        className="absolute z-0 w-[15rem] aspect-square bg-primary rounded-full top-[170%] left-0 translate-x-[-50%] translate-y-[-50%] blur-[8rem]"
-        ></div>
-    <HistorySection />
-    <Footer/>
+      <Navbar />
+      <PakDukuhShowcase />
+      <div className="absolute z-0 w-[15rem] aspect-square bg-primary rounded-full top-[150%] right-0 translate-x-[-50%] translate-y-[-50%] blur-[8rem]"></div>
+      <div className="absolute z-0 w-[15rem] aspect-square bg-primary rounded-full top-[170%] left-0 translate-x-[-50%] translate-y-[-50%] blur-[8rem]"></div>
+      <HistorySection />
+      <div className="flex justify-center items-center w-3/4 mx-auto flex-col gap-5">
+        <p className="font-poppins text-3xl lg:text-5xl font-bold">
+          Peta Lengkap Wotawati
+        </p>
+        <img src={mapping} alt="" className="" />
+      </div>
+
+      <Footer />
     </ReactLenis>
   );
 };
